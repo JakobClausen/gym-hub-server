1cc750731f536dbe7f32bb7f4e855839717cdeba
JakobClausen <jakob.fridmar@gmail.com>
2021-08-09 21:47:01 +0200

    add update mutation

--- src/resolvers/GymClassResolver.ts
+++ src/resolvers/GymClassResolver.ts
@@ -1,3 +1,4 @@
+import { UserInputError } from 'apollo-server-express';
 import {
   Arg,
   Ctx,
@@ -8,20 +9,38 @@ import {
 } from 'type-graphql';
 import { Context } from '../context/prisma';
 import { isAuth } from '../middleware/isAuth';
-import { AddGymClass, GymClass } from '../schema/GymClass';
+import { AddGymClass, GymClass, UpdateGymClass } from '../schema/GymClass';
 
 @Resolver(GymClass)
 export class GymClassResolver {
   @Query(() => [GymClass])
   @UseMiddleware(isAuth)
-  async classes(
-    @Ctx() ctx: Context,
-    @Arg('day', { nullable: true }) day?: number
-  ) {
+  async classes(@Ctx() ctx: Context, @Arg('day') day: number) {
     return ctx.prisma.gymClass.findMany({
       where: { gymId: ctx.payload.gymId, dayOfTheWeek: day },
       orderBy: { startTime: 'asc' },
     });
+  }
+
+  @Mutation(() => GymClass)
+  @UseMiddleware(isAuth)
+  async updateGymClass(
+    @Arg('id') id: number,
+    @Arg('updateGymClass') input: UpdateGymClass,
+    @Ctx() ctx: Context
+  ) {
+    try {
+      return await ctx.prisma.gymClass.update({
+        where: {
+          id,
+        },
+        data: {
+          ...input,
+        },
+      });
+    } catch (error) {
+      throw new UserInputError('Something went wrong!');
+    }
   }
 
   @Mutation(() => GymClass)
